5707fd1f6703d6b708e5fafcd8c8e24f2e79c2de
João Brentano <joaobrentano.br@gmail.com>
2022-03-21 15:13:33 -0300

    Created base

---
+++ MovieDB OBJC/Model/Movie.h
@@ -0,0 +1,13 @@
+//
+//  Movie.h
+//  MovieDB OBJC
+//
+//  Created by João Brentano on 21/03/22.
+//
+
+#ifndef Movie_h
+#define Movie_h
+
+
+
+#endif /* Movie_h */
---
+++ MovieDB OBJC/Model/Movie.m
@@ -0,0 +1,10 @@
+//
+//  Movie.m
+//  MovieDB OBJC
+//
+//  Created by João Brentano on 21/03/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "Movie.h"
+
